e55bf364c68d5d4d6943fa5d92e5a5a37b077a79
Henry Sabio <hsabio.a@gmail.com>
2021-05-26 01:27:57 -0400

    reverted boostrap nav

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,33 +1,35 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light container d-flex justify-content-between py-4">
-    <div class="container-fluid">
-        <a href="index.php" class="navbar-brand company-logo"><img src="img/logo.svg" alt="Velocity Logo"></a>
-        <button class="navbar-toggler" data-target="#navbarResponsive" data-toggle="collapse" type="button">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarResponsive">
-            <ul id="navbar" class="navbar-nav ml-auto ">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Overview</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="index.php">News & Events</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Company Info</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Financial Info</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Stock Info</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">SEC Filings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Governance</a>
-                </li>
-            </ul>
+<div class="container-fluid nav-container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container d-flex justify-content-between py-4">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand company-logo"><img src="img/logo.svg" alt="Velocity Logo"></a>
+            <button class="navbar-toggler" data-target="#navbarResponsive" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarResponsive">
+                <ul id="navbar" class="navbar-nav ml-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Overview</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">News & Events</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Company Info</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Financial Info</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Stock Info</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SEC Filings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Governance</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
